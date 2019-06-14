0f31f2961e8f56f032269fc3ff7b2f79b950fde2
AtomskX <rlamezquita@gmail.com>
2019-06-14 10:58:34 -1000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                
+        String test = "23*";
+        char[] charArray = (test.toCharArray());
+        
+        
+        
+        Evaluation.Evaluation(charArray);
+        
+        
+    }
+    
+}
