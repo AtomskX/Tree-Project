083de656d86f298414433fa5ab02118edaae653f
AtomskX <rlamezquita@gmail.com>
2019-06-14 11:01:47 -1000

    Create OperatorNode.java

---
+++ OperatorNode.java
@@ -0,0 +1,11 @@
+public class OperatorNode extends Node{
+    
+    //constructor for the operator node
+    public OperatorNode(char value){
+        
+        super(value);
+        
+    }//end OperatorNode constructor
+    
+    
+}// end OperatorNode class
